f84fdd6953f2c02ad40892192073d97a347d1cfe
Joel Spadin <joelspadin@gmail.com>
2023-04-11 11:38:05 -0500

    Update config

--- config.h
+++ config.h
@@ -1,1 +1,4 @@
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Debug stuff.
+// #define DEBUG_MATRIX_SCAN_RATE
--- rules.mk
+++ rules.mk
@@ -2,6 +2,10 @@ SRC += source.c
 
 CAPS_WORD_ENABLE = yes
 DEBOUNCE_TYPE = asym_eager_defer_pk
+DEBOUNCE = 7
 EXTRAKEY_ENABLE = yes
 NKRO_ENABLE = yes
 VIA_ENABLE = yes
+
+# Debug stuff.
+# CONSOLE_ENABLE = yes
