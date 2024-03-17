dff2d3d14e39b6ba469c0cfc0aced3a0266d215d
Joel Spadin <joelspadin@gmail.com>
2024-03-17 11:39:03 -0500

    Try longer debounce

--- rules.mk
+++ rules.mk
@@ -2,10 +2,11 @@ SRC += source.c
 
 CAPS_WORD_ENABLE = yes
 DEBOUNCE_TYPE = asym_eager_defer_pk
-DEBOUNCE = 7
+DEBOUNCE = 10
 EXTRAKEY_ENABLE = yes
 NKRO_ENABLE = yes
 VIA_ENABLE = yes
 
 # Debug stuff.
 # CONSOLE_ENABLE = yes
+# COMMAND_ENABLE = yes
